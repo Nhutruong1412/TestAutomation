e2fe2d34a6cdd80bada0ef93efcd1bc226c47259
Nhu Truong <truonghuynhnhu1412@gmail.com>
2021-04-29 11:35:57 +0700

    Open Chrome and does not unsafe link

--- src/test/java/library/Home.java
+++ src/test/java/library/Home.java
@@ -1,22 +1,28 @@
+
+
 package library;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Instant;
 
 public class Home {
     WebDriver driver;
 
+
     public Home(WebDriver driver) {
         this.driver = driver;
+
     }
 
     //Locator on the page https://demo.guru99.com/
     public By detailButton = By.xpath("//button[@id='details-button']");
     public By proceedLink = By.xpath("//a[@id='proceed-link']");
     //Locator on the first page
+    public By emailID = By.xpath("//td[contains(.,'Email ID')]");
     public By emailField = By.xpath("//input[@type='text']");
     public By submitButton = By.xpath("//input[@type='submit']");
     public By userID = By.xpath("(//td[@align='center'])[3]");
@@ -29,22 +35,25 @@ public class Home {
     public By userID_field = By.xpath("//input[@name='uid']");
     public By passWord_field = By.xpath("//input[@name='password']");
 
-
-
+    public class ChromeOptions {
+    }
     public void AccessToHomePage() {
         driver.findElement(detailButton).click();
 //   WebDriverWait wait = new WebDriverWait(driver,20);
 //    wait.until(ExpectedConditions.visibilityOf((WebElement) proceed_link));
         driver.findElement(proceedLink).click();
     }
-    public void EnterEmail (String strInput){
+
+    public void EnterEmail(String strInput) {
+        WebDriverWait wait = new WebDriverWait(driver,20);
+        wait.until(ExpectedConditions.elementToBeClickable(emailID));
         driver.findElement(emailField).click();
         driver.findElement(emailField).sendKeys(strInput);
         driver.findElement(submitButton).click();
     }
 
-    public void Login (String user, String pass){
-        WebDriverWait wait = new WebDriverWait(driver,20);
+    public void Login(String user, String pass) {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
         wait.until(ExpectedConditions.elementToBeClickable(userID_field));
         driver.findElement(userID_field).click();
         driver.findElement(userID_field).sendKeys();
--- src/test/java/scripts/CreateAccount.java
+++ src/test/java/scripts/CreateAccount.java
@@ -5,32 +5,46 @@ import library.Home;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class CreateAccount {
+
+    private Object ChromeOptions;
 
     @Test
     public void getAccount() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--ignore-certificate-errors");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(driver,10);
         driver.get("https://demo.guru99.com/");
         Home home = new Home(driver);
-        home.AccessToHomePage();
+        //home.AccessToHomePage();
         home.EnterEmail("truonghuynhnhu1412@gmail.com");
 
     }
-   @Test
+    @Test
     public void Account() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://demo.guru99.com/v4");
         Home home = new Home(driver);
-       home.AccessToHomePage();
+        home.AccessToHomePage();
         String ID = driver.findElement(By.xpath("(//td[@align='center'])[3]")).getText();
         String PWord = driver.findElement(By.xpath("(//td[@align='center'])[5]")).getText();
         home.Login(ID, PWord);
     }
 
 }
-
-
