7b0e2a1e229255683e110449426f4b604893c199
Nhu Truong <truonghuynhnhu1412@gmail.com>
2021-04-29 11:51:44 +0700

    1 Test case for login

--- src/test/java/scripts/CreateAccount.java
+++ src/test/java/scripts/CreateAccount.java
@@ -22,29 +22,18 @@ public class CreateAccount {
     public void getAccount() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-
-
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--ignore-certificate-errors");
         driver = new ChromeDriver(chromeOptions);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        WebDriverWait wait = new WebDriverWait(driver,10);
         driver.get("https://demo.guru99.com/");
         Home home = new Home(driver);
-        //home.AccessToHomePage();
         home.EnterEmail("truonghuynhnhu1412@gmail.com");
-
-    }
-    @Test
-    public void Account() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://demo.guru99.com/v4");
-        Home home = new Home(driver);
-        home.AccessToHomePage();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(driver,10);
         String ID = driver.findElement(By.xpath("(//td[@align='center'])[3]")).getText();
         String PWord = driver.findElement(By.xpath("(//td[@align='center'])[5]")).getText();
         home.Login(ID, PWord);
+
     }
 
 }
